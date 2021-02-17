890d57487d21e9921f10da05fc9ea99f1628bebd
Ukuyoma Theophilus <theo4biz2012@gmail.com>
2021-02-18 01:27:23 +0300

    fix: uploader

--- src/utils/uploader.js
+++ src/utils/uploader.js
@@ -3,33 +3,17 @@ import path from 'path';
 import colors from 'colors';
 
 export default function uploader(destination) {
-  if (process.env.NODE_ENV === 'production') {
-    console.log(path.resolve(__dirname, 'build'));
-    var storage = multer.diskStorage({
-      destination(req, file, cb) {
-        cb(null, path.resolve(__dirname, 'build'));
-      },
-      filename(req, file, cb) {
-        cb(
-          null,
-          `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        );
-      },
-    });
-  } else {
-    var storage = multer.diskStorage({
-      destination(req, file, cb) {
-        cb(null, path.resolve(__dirname, 'uploads'));
-      },
-      filename(req, file, cb) {
-        cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
-        cb(
-          null,
-          `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        );
-      },
-    });
-  }
+  const storage = multer.diskStorage({
+    destination(req, file, cb) {
+      cb(null, '');
+    },
+    filename(req, file, cb) {
+      cb(
+        null,
+        `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+      );
+    },
+  });
 
   function checkFileType(file, cb) {
     const filetypes = /jpg|jpeg|png/;
@@ -54,47 +38,3 @@ export default function uploader(destination) {
 
   return upload;
 }
-
-console.log(path.resolve(__dirname, 'build'));
-// https://stackoverflow.com/questions/50566277/uploading-image-to-heroku-using-node-and-multer-not-work
-// const multer = require('multer');
-// const path = require('path');
-
-// if (process.env.NODE_ENV === 'production') {
-//   var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, path.resolve(__dirname, 'build'));
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
-//     },
-//   });
-// } else {
-//   var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, path.resolve(__dirname, 'uploads'));
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
-//     },
-//   });
-// }
-
-// const uploads = multer({ storage: storage });
-
-// app.use(uploads.any());
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.resolve(__dirname, 'build')));
-// } else {
-//   app.use(express.static('./public'));
-// }
-
-// //if you need to download (after upload) files in cloudinary
-// const cloudinary = require('cloudinary');
-// cloudinary.config({
-//   cloud_name: '...',
-//   api_key: '...',
-//   api_secret: '...',
-// });
-// //if you need to del files after upload
-// const fs = require('fs');
